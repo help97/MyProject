67b980a5eca29d0769d6df43e707f37eb9a624de
help97 <1204998502@qq.com>
2019-12-02 11:12:27 +0800

    修改测试

--- src/main/java/com/tedu/pojo/TestGIT.java
+++ src/main/java/com/tedu/pojo/TestGIT.java
@@ -3,5 +3,8 @@ package com.tedu.pojo;
 public class TestGIT {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
