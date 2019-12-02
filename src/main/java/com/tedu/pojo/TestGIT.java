fe9b1513190cb41146f5b8086c8beeb37ed02efa
help97 <1204998502@qq.com>
2019-12-02 11:07:50 +0800

    Merge branch 'master' of E:\java_work\IdeaProjects\EsayMall with conflicts.

--- src/main/java/com/tedu/pojo/TestGIT.java
+++ src/main/java/com/tedu/pojo/TestGIT.java
@@ -3,7 +3,5 @@ package com.tedu.pojo;
 public class TestGIT {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-
     }
 }
